34fda3346b1e22db2543978ffd420357d2857abc
Callie Rose Petal <dev@lonelycarp.com>
2025-10-26 15:27:22 +0000

    mv index

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({"assets": "assets"});
-    eleventyConfig.addWatchTarget("assets");
-  
-    return {
-      dir: {
-        input: "content",
-        includes: "../_includes",
-        data: "_data",
-        output: "_site"
-      },
-      markdownTemplateEngine: "njk",
-      htmlTemplateEngine: "njk",
-      templateFormats: ["md", "njk", "html"]
-    };
+  // Copy assets folder to output
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+    dir: {
+      input: "content",       // Look for pages in /content
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    templateFormats: ["njk", "md", "html"]
   };
+};
