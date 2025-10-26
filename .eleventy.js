352d49d41f0576170eaf6b794e03056de0e3598f
Callie Rose Petal <dev@lonelycarp.com>
2025-10-26 15:19:31 +0000

    routing

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,8 @@ module.exports = function(eleventyConfig) {
   
     return {
       dir: {
-        input: ".",
-        includes: "_includes",
+        input: "content",
+        includes: "../_includes",
         data: "_data",
         output: "_site"
       },
