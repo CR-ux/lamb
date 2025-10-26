995c64dce6c4ba12e3c7596afa02cc5f8017093e
Callie Rose Petal <dev@lonelycarp.com>
2025-10-26 12:39:04 +0000

    data dir

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function(eleventyConfig) {
         dir: {
         input: ".",
         includes: "./_includes",
-        data: "../_data",
+        data: "./_data",
         output: "_site"
         },
         markdownTemplateEngine: "njk",
