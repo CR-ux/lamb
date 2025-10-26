f182836569df652a942c2917547e7986007458d7
Callie Rose Petal <dev@lonelycarp.com>
2025-10-26 12:29:37 +0000

    njk

--- .eleventy.js
+++ .eleventy.js
@@ -2,15 +2,15 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({"assets": "assets"});
     eleventyConfig.addPassthroughCopy({"functions": "functions"}); // CF Functions
     eleventyConfig.addWatchTarget("assets/css/ritual.css");
-  
+
     return {
-      dir: {
+        dir: {
         input: "content",
         includes: "../_includes",
         data: "../_data",
         output: "_site"
-      },
-      markdownTemplateEngine: "njk",
-      htmlTemplateEngine: "njk"
+        },
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk"
+        };
     };
-  };
